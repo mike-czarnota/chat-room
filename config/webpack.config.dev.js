8fab8a886e7689846fd726da374263c483fa239b
panczarny <czarny1906@o2.pl>
2018-08-11 00:16:33 +0200

    Added sass to project

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -151,6 +151,15 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          // sass loader
+          {
+            test: /\.sass$/,
+            use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+            ]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
